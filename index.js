4e3e4fcd4d5681aeba4bbae0042fe4696190bd58
Maxime Petit <mpetithengen@gmail.com>
2017-01-23 20:12:08 +0100

    Update: index.js

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', function (socket) {
     var clientIp = socket.request.connection.remoteAddress;
 
     console.log(clientIp + ' connected');
     socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
+        console.log(clientIp + ' sent: ' + msg);
         io.emit('chat message', msg);
     });
     socket.on('disconnect', function () {
         console.log(clientIp + ' deconnected');
-    });    
+    });
 	socket.on('update loc', function () {
         console.log(clientIp + ' updated their location');
-    });
+    });    
 });
 
 var port = process.env.PORT || 3000;
 
 http.listen(port, function () {
-    console.log('listening on port ' + port);
-	console.log( http.address().address);
+    console.log('listening on *:3000');
 });
