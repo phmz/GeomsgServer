12937ab5ead07465ac382b3fd750365c1941125a
Maxime Petit <mpetithengen@gmail.com>
2017-01-26 13:30:43 +0100

    Add: server now keeps track of all user online

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = new Map();
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -15,7 +16,7 @@ io.on('connection', function (socket) {
     console.log(clientIp + ' connected');
     socket.on('chat message', function (data) {
         var data = data;
-        console.log(data.fromUser + ' SENT A MESSAGE TO '+ data.toUser + '\n' + data.message);
+        console.log(data.fromUser + ' SENT A MESSAGE TO ' + data.toUser + '\n' + data.message);
         //console.log(clientIp + ' sent: ' + msg);
         io.emit('chat message', "delivered");
     });
@@ -25,7 +26,18 @@ io.on('connection', function (socket) {
     socket.on('update loc', function (data) {
         var data = data;
         console.log(clientIp + ' updated their location');
-        console.log('new location, user : '+data.name+' change position -- latitude = ' + data.latitude + ', longitude = ' + data.longitude);
+        console.log('new location, user : ' + data.name + ' change position -- latitude = ' + data.latitude + ', longitude = ' + data.longitude);
+    });
+    socket.on('new connection', function (userId, data) {
+        var data = data;
+        var userId = userId;
+        users.set(userId, data);
+        console.log('New user connected: ' + userId + ' ' + users.get(userId).latitude + ' ' + users.get(userId).longitude);
+        console.log(users.size + ' users currently connected');
+        getUserList(userId, users.get(userId).latitude, users.get(userId).longitude);
+    });
+    socket.on('request list', function () {
+
     });
 });
 
@@ -34,3 +46,51 @@ var port = process.env.PORT || 3000;
 http.listen(port, function () {
     console.log('listening on : ' + port);
 });
+
+function getUserList(userId, latitude, longitude) {
+    var jsonList = "";
+    users.forEach(function (value, key) {
+        if (userId !== key) {
+            var userJson = userToJsonString(userId, latitude, longitude);
+            console.log(distanceBetween(latitude, longitude, value.latitude, value.longitude) + " meters");
+            jsonList = appendUserToJson(jsonList, userJson);
+        }
+    });
+    jsonList = closeUserListJson(jsonList);
+    console.log(jsonList);
+}
+
+function distanceBetween(lat1, lon1, lat2, lon2) {
+    var R = 6371e3; // metres
+    var φ1 = lat1.toRadians();
+    var φ2 = lat2.toRadians();
+    var Δφ = (lat2 - lat1).toRadians();
+    var Δλ = (lon2 - lon1).toRadians();
+
+    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    var d = R * c;
+    return d;
+}
+
+if (Number.prototype.toRadians === undefined) {
+    Number.prototype.toRadians = function() { return this * Math.PI / 180; };
+}
+
+function userToJsonString(userId, latitude, longitude) {
+    return "{name:" + userId + ",latitude:" + latitude + ",longitude:" + longitude + "}";
+}
+
+function appendUserToJson(completeJsonString, userJsonString) {
+    if(completeJsonString === "") {
+        return completeJsonString.concat(userJsonString);
+    }
+    return completeJsonString.concat(',').concat(userJsonString);
+}
+
+function closeUserListJson(jsonList) {
+    return '{'.concat(jsonList).concat('}');
+}
